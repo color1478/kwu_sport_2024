47be6a7a35d676ffcf62a3be4ce6a5ecb8e19912
color1478 <jehohi04@naver.com>
2024-08-12 08:07:21 +0900

    배너 변경 및 종목별 항목 추가

--- js/result.js
+++ js/result.js
@@ -1,8 +1,7 @@
 document.getElementById("result_sports-selection").addEventListener("click", function() {
-    window.location.href = "result_sports.html";
+    window.location.href = "./sports_result/풋살.html";
 });
 
 document.getElementById("rank-selection").addEventListener("click", function() {
-    window.location.href = "results.html";
+    window.location.href = "../results.html";
 });
-
