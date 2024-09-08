3c3a737d5da6142855b41c481e99beb69a84015c
unknown <jehohi04@naver.com>
2024-09-09 01:48:20 +0900

    rules download button add

--- js/gamedata.js
+++ js/gamedata.js
@@ -21,25 +21,144 @@ var sportsAndLocations = {
     // 필요한 종목과 장소 추가
 };
 var games = [
-    { // 일반 종목
-        date: "2024-09-07",
-        time: "10:00",
-        sportId: 2,
+    { // 3교시 경기
+        date: "2024-09-09",
+        time: "3교시",
+        sportId: 1,
         home: {
-            affiliation: "인문사회과학대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : true
+            affiliation: "인공지능융합대학",
+            team: "로봇학부",
+            abstention: false
         },
         away: {
-            affiliation: "인공지능융합대학",
-            team: "소프트웨어학부",
-            abstention : false
+            affiliation: "전자정보공과대학",
+            team: "전자재료공학과",
+            abstention: false
         },
-        home_score: 0,
-        away_score: 5,
-        result: "원정팀 승",
-        rain: true
+        home_score: '-',
+        away_score: '-',
+        result: "-",
+        rain: false
     },
+    { // 4교시 경기
+        date: "2024-09-09",
+        time: "4교시",
+        sportId: 1,
+        home: {
+            affiliation: "정책법학대학",
+            team: "법학부",
+            abstention: false
+        },
+        away: {
+            affiliation: "정책법학대학",
+            team: "행정학과",
+            abstention: false
+        },
+        home_score: '-',
+        away_score: '-',
+        result: "-",
+        rain: false
+    },
+    { // 5교시 경기
+        date: "2024-09-09",
+        time: "5교시",
+        sportId: 1,
+        home: {
+            affiliation: "자연과학대학",
+            team: "화학과",
+            abstention: false
+        },
+        away: {
+            affiliation: "전자정보공과대학",
+            team: "전자공학과",
+            abstention: false
+        },
+        home_score: '-',
+        away_score: '-',
+        result: "-",
+        rain: false
+    },
+    { // 6교시 경기
+        date: "2024-09-09",
+        time: "6교시",
+        sportId: 1,
+        home: {
+            affiliation: "전자정보공과대학",
+            team: "전자융합공학과",
+            abstention: false
+        },
+        away: {
+            affiliation: "경영대학",
+            team: "국제통상학부",
+            abstention: false
+        },
+        home_score: '-',
+        away_score: '-',
+        result: "-",
+        rain: false
+    },
+    { // 4교시 경기
+        date: "2024-09-09",
+        time: "4교시",
+        sportId: 4,
+        home: {
+            affiliation: "공과대학",
+            team: "화학공학과",
+            abstention: false
+        },
+        away: {
+            affiliation: "경영대학",
+            team: "국제통상학부",
+            abstention: false
+        },
+        home_score: '-',
+        away_score: '-',
+        result: "-",
+        rain: false
+    },
+    { // 5교시 경기
+        date: "2024-09-09",
+        time: "5교시",
+        sportId: 4,
+        home: {
+            affiliation: "정책법학대학",
+            team: "법학부",
+            abstention: false
+        },
+        away: {
+            affiliation: "정책법학대학",
+            team: "국제학부",
+            abstention: false
+        },
+        home_score: '-',
+        away_score: '-',
+        result: "-",
+        rain: false
+    },
+    { // 6교시 경기
+        date: "2024-09-09",
+        time: "6교시",
+        sportId: 4,
+        home: {
+            affiliation: "인공지능융합대학",
+            team: "정보융합학부",
+            abstention: false
+        },
+        away: {
+            affiliation: "공과대학",
+            team: "건축공학과",
+            abstention: false
+        },
+        home_score: '-',
+        away_score: '-',
+        result: "-",
+        rain: false
+    },
+
+
+
+
+
     { //tft
         date: "2024-09-07",
         time: "10:00",
@@ -59,365 +178,5 @@ var games = [
         result: "홈팀 승",
         rain: true
     },
-    {
-        date: "2024-09-07",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "공과대학",
-            team: "화학공학과",
-            abstention : true
-        },
-        away: {
-            affiliation: "자연과학대학",
-            team: "화학과",
-            abstention : false
-        },
-        home_score: 0,
-        away_score: 3,
-        result: "원정팀 승",
-        isActive: false
-    },
-    {
-        date: "2024-08-03",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "경영대학",
-            team: "경영학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "인문사회과학대학",
-            team: "영어산업학과",
-            abstention : false
-        },
-        home_score: 1,
-        away_score: 2,
-        result: "원정팀 승",
-        rain: true
-    },
-    {
-        date: "2024-08-04",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "정책법학대학",
-            team: "행정학과",
-            abstention : false
-        },
-        away: {
-            affiliation: "자연과학대학",
-            team: "수학과",
-            abstention : false
-        },
-        home_score: 2,
-        away_score: 2,
-        result: "무승부",
-        rain: false
-    },
-    {
-        date: "2024-08-04",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "인공지능융합대학",
-            team: "소프트웨어학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "전자정보공과대학",
-            team: "전자공학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 1,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-05",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "자연과학대학",
-            team: "물리학과",
-            abstention : false
-        },
-        away: {
-            affiliation: "공과대학",
-            team: "기계공학과",
-            abstention : false
-        },
-        home_score: 1,
-        away_score: 4,
-        result: "원정팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-05",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "경영대학",
-            team: "회계학과",
-            abstention : false
-        },
-        away: {
-            affiliation: "정책법학대학",
-            team: "법학과",
-            abstention : false
-        },
-        home_score: 0,
-        away_score: 0,
-        result: "무승부",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "인문사회과학대학",
-            team: "사회학과",
-            abstention : false
-        },
-        away: {
-            affiliation: "자연과학대학",
-            team: "생물학과",
-            abstention : false
-        },
-        home_score: 2,
-        away_score: 3,
-        result: "원정팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 1,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 3,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 4,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 5,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 6,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 7,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 8,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 10,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-08-16",
-        time: "10:00",
-        sportId: 2,
-        home: {
-            affiliation: "공과대학",
-            team: "미디어커뮤니케이션학부",
-            abstention : false
-        },
-        away: {
-            affiliation: "경영대학",
-            team: "마케팅학과",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 2,
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-09-01",
-        time: "1교시",
-        sportId: 10,
-        home: {
-            affiliation: "자연과학대학",
-            team: "스포츠융합과학과",
-            abstention : false
-        },
-        away: {
-            affiliation: "인공지능융합대학",
-            team: "정보융합학부",
-            abstention : false
-        },
-        home_score: '-',
-        away_score: '-',
-        result: "홈팀 승",
-        rain: false
-    },
-    {
-        date: "2024-09-01",
-        time: "3교시",
-        sportId: 2,
-        home: {
-            affiliation: "공과대학",
-            team: "건축학과",
-            abstention : false
-        },
-        away: {
-            affiliation: "인공지능융합대학",
-            team: "소프트웨어학부",
-            abstention : false
-        },
-        home_score: 3,
-        away_score: 23,
-        result: "홈팀 승",
-        rain: false
-    }
+   
 ];
