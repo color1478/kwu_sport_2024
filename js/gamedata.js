92936cc25cd6d4fc670218dbeda40361a21add8f
elisaohh <elisaohh@gmail.com>
2024-09-25 09:18:21 +0900

    재수정

--- js/gamedata.js
+++ js/gamedata.js
@@ -1665,7 +1665,7 @@ var games = [
         away: {
             affiliation: "인공지능융합대학",
             team: "컴퓨터정보공학부",
-            abstention: true
+            abstention: false
         },
         home_score: '-',
         away_score: '-',
